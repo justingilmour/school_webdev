5983f1bc4e5791e988bcf8de654cb2f91f6846dd
Justin Gilmour <justin.gilmour@my.simpson.edu>
2018-10-30 09:18:32 -0500

    changed navbar

--- public_html/resources/navigation.php
+++ public_html/resources/navigation.php
@@ -1,7 +1,32 @@
-<ul class="nav navbar-nav">
-	<li><a href="index.html">Home</a></li>
-	<li><a href="store.html">Store</a></li>
-	<li><a href="about.html">Contact Us</a></li>
-	<li><a href="login.html">Login</a></li>
-	<li><a href="signup.html">Sign-Up</a></li>
-</ul>
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="index.html"><img src="img/new_logo.png" alt="H2O Empire"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="store.html">Store</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">Contact Us</a>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Login</a>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item" href="login.html">Login</a>
+                  <a class="dropdown-item" href="signup.html">Signup</a>
+                </div>
+              </li>
+            </ul>
+
+          </div>
+        </nav>
