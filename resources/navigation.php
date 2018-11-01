5ace857b83cf60b47cca819e966f2a20e9f02ed7
Justin Gilmour <justin.gilmour@my.simpson.edu>
2018-11-01 08:26:40 -0500

    Reverted navbar

--- resources/navigation.php
+++ resources/navigation.php
@@ -1,32 +1,31 @@
-     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a href="index.html"><img src="img/new_logo.png" alt="H2O Empire"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
             <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="store.html">Store</a>
+              </li>
+
               <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="store.php">Store</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="about.php">Contact Us</a>
+                <a class="nav-link" href="about.html">Contact Us</a>
               </li>
 
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Login</a>
                 <div class="dropdown-menu">
-                  <a class="dropdown-item" href="login.php">Login</a>
-                  <a class="dropdown-item" href="signup.php">Signup</a>
-                </div>
-              </li>
+                  <a class="dropdown-item" href="login.html">Login</a>
+                  <a class="dropdown-item" href="signup.html">Signup</a>
+               </div>
+             </li>
             </ul>
 
-          </div>
-        </nav>
+        </div>
+       </nav>
